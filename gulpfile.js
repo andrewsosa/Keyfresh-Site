f0d336452efeadfeac23f9df7cb202e7da84b831
andrewsosa001 <andrewsosa001@gmail.com>
2017-02-23 09:48:14 -0500

    Update project config

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,34 @@
 var gulp = require('gulp');
+var pug = require('gulp-pug');
 var sass = require('gulp-sass');
-var pug  = require('gulp-pug');
 
-var dest = 'html';
+var html_dest = 'server/app/static/templates';
+var sass_test = "server/app/static/css";
 
 // Compiles .pug files into .html
 gulp.task('pug', function() {
-  return gulp.src('src/*.pug')
-    .pipe(pug())
-	  .pipe(gulp.dest(dest)); // tell gulp our output folder
+	gulp.src('src/**.pug')
+		.pipe(pug())
+		.pipe(gulp.dest(html_dest)); // tell gulp our output folder
 });
 
 // Compiles .sass into .css
 gulp.task('sass', function() {
-	gulp.src('src/sass/*.sass')
+	gulp.src('src/sass/**.sass')
 		.pipe(sass().on('error', sass.logError))
-    	.pipe(gulp.dest(dest + '/css/'));
+		.pipe(gulp.dest(sass_dest));
 });
+
+//
+gulp.task('build', ['pug', 'sass']);
 
 // Monitors .pug and .sass
 gulp.task('watch', function() {
 	gulp.watch('src/**', ['pug', 'sass']);
 });
 
-gulp.task('test', function() {
+//
+gulp.task('default', function() {
   	// place code for your default task here
-	console.log("hello, world!")
+	console.log("Hi, I'm Gulp!")
 });
-
-gulp.task('default', ['watch']);
